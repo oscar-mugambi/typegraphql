3d9bbff664dee6ef6bccbf249cf045493f87c364
oscar-mugambi <oscarmugambi@gmail.com>
2022-04-09 13:54:32 +0300

    added first typegrapql schema

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { buildSchema, Query, Resolver } from 'type-graphql';
+
+@Resolver()
+class HelloResolver {
+  // we have a field for querying
+  @Query(() => String) // uppercase string because we need to pass the class of the string
+  // name of the function is the name of the query
+  async hello() {
+    return await 'Hello World!!';
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = express();
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('server started on 4000');
+  });
+};
+
+main();
